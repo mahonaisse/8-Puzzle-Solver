f71310fd0a41ad213d907c38966774258bc219d3
mahonaisse <jonsquared4@gmail.com>
2024-10-28 12:23:35 -0700

    Add Tree class with empty methods and members for state expansion.

--- Tree.cpp
+++ Tree.cpp
@@ -1,1 +1,12 @@
 #include "Tree.h"
+
+void uniform_cost_search() {
+
+};
+void a_star_search_with_misplaced_tiles() {
+
+};
+
+void a_star_search_with_euclidean_distance() {
+    
+};
--- Tree.h
+++ Tree.h
@@ -1,8 +1,24 @@
 #ifndef TREE_H_
 #define TREE_H_
 
-class Tree {
+#include "Problem.h"
 
+class Tree {
+    public:
+        // Constructor to initialize root with problem
+        Tree(Problem p) : root(&p), left(nullptr), right(nullptr) {}
+
+        void uniform_cost_search();
+        void a_star_search_with_misplaced_tiles();
+        void a_star_search_with_euclidean_distance();
+    private:
+        Problem* root;
+        Problem* left;
+        Problem* right;
+
+        int heuristic_cost_;
+        int actions_cost_;
+        int max_number_of_queued_nodes_;
 };
 
-#endif
+#endif  // TREE_H_
